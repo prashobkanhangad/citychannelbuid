47b8d978910ef89dec6d47dad1c9f374e9853b17
prashobkanhangad <info.prashob@gmail.com>
2023-07-03 18:27:39 +0530

    da

---
+++ clappr_html_view.dart.js
@@ -0,0 +1,8 @@
+var player = new Clappr.Player({
+    source: 'https://5a1178b42cc03.streamlock.net:443/8110/8110/playlist.m3u8',
+    parentId: '#clappr',
+    width: '100%',
+    height: '100%',
+    autoPlay: false
+  });
+  
